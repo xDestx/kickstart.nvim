793dded4e3419ce90823f02a586d2416e294b5eb
xdest <destroyer7712@gmail.com>
2026-02-15 23:34:16 -0500

    Changes

--- init.lua
+++ init.lua
@@ -257,7 +257,6 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
   { 'NMAC427/guess-indent.nvim', opts = {} },
-
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
